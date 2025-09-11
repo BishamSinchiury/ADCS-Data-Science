e3d8b409978d319306553e61237e0c62f7f89b89
BishamSinchiury <bishamsinchiury1116@gmail.com>
2025-09-11 15:18:37 +0545

    new

---
+++ import_export.py
@@ -0,0 +1,11 @@
+import pandas as pd
+# Importing pandas library to use its functions
+# As pd for easier reference and calling 
+ 
+
+csv_data = pd.read_csv("data/employee.csv")
+#Import csv file with read_csv() function
+# syntax: pd.read_csv(filepath)
+ 
+print("CSV Imported: \n", csv_data.head())
+
